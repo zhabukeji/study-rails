6a5bf11feb907845032cb6a10192a2ab605549f0
zhabukeji@foxmail.com <renzhabu@foxmail.com>
2024-07-23 00:40:49 +0800

    重做article模块

--- app/controllers/article_controller.rb
+++
@@ -1,5 +0,0 @@
-class ArticleController < ApplicationController
-  def index
-    
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
+  resources :articles
   root 'site#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  get '/article(/:id)' , to: "article#index" , as: "article"
 end

13a9fcff56b63212eabe387d3f936f3b1c880d7f
zhabukeji@foxmail.com <renzhabu@foxmail.com>
2024-07-25 09:53:53 +0800

    尝试折腾i18n

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ApplicationRecord
+    validates :title, :author, :context, presence: true
+
     def self.index_list
         self.select(:id, :title, :author, :thumbnail, :context, :created_at).order(created_at: :desc).take(3)
     end

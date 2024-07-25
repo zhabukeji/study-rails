1c7c7f4c304e56b491f3700977a67895724b42fa
zhabukeji@foxmail.com <renzhabu@foxmail.com>
2024-07-25 11:04:19 +0800

    优化显示

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,5 +7,5 @@
 #   Character.create(name: "Luke", movie: movies.first)
 #   Topbar.create([{ name: "主页" , address: "site"}, { name: "文章" , address: "#"} ,{ name: "时间线" , address: "#"}])
 
-#   Article.create( title: "总在思考一句积极的话" , author: "amazeUI" , context: "那时候刚好下着雨，柏油路面湿冷冷的，还闪烁着青、黄、红颜色的灯火。" ,thumbnail: "/assets/i/f10.jpg")
-Quote.create( [{content:"听过很多大道理，但依然过不好这一生"},{content: "想要变强"}])
+Article.create( title: "总在思考一句积极的话" , author: "amazeUI" , context: "那时候刚好下着雨，柏油路面湿冷冷的，还闪烁着青、黄、红颜色的灯火。" ,thumbnail: "/assets/i/f10.jpg")
+#   Quote.create( [{content:"听过很多大道理，但依然过不好这一生"},{content: "想要变强"}])

2e78ac13b4765b5d9fdea05e4a8e243da91fb0d4
zhabukeji@foxmail.com <renzhabu@foxmail.com>
2024-07-29 15:14:13 +0800

    准备开发后台

---
+++ app/controllers/admin/site_controller.rb
@@ -0,0 +1,2 @@
+class Admin::SiteController < ApplicationController
+end
---
+++ test/controllers/admin/site_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class Admin::SiteControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
